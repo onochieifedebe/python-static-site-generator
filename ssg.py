4bf2aae14dbdff610a667478dadf28feb67cf8eb
Onochie Ifedebe <ifedebe.o@gmail.com>
2021-08-29 16:48:21 -0500

    "module1 site class modification"

--- ssg.py
+++ ssg.py
@@ -3,12 +3,9 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {
-        "source": source,
-        "dest": dest
-    }
+    config = {"source": source, "dest": dest}
 
     Site(**config).build()
 
 
-typer.run(main())
+typer.run(main)
